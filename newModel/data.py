6fe309821e4401180c82fd2f3313cee715afe4b3
Noah <sewellnoah@gmail.com>
2021-11-19 13:20:44 -0500

    splitting of data to training and test

--- newModel/data.py
+++ newModel/data.py
@@ -1,19 +1,54 @@
 import requests
 import json
 import csv
+from pandas_datareader import data
+import pandas as pd
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
+#Alphavantage API usages
 key = 'D57064T4IGI17THK'
 ticker = 'FB'
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}&datatype=csv'.format(ticker, key)
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&outputsize=full&apikey={}&datatype=csv'.format(ticker, key)
 response = requests.get(url)
+
+#convert to Response object to text for file writing.
 data = response.text
-#print(response.json())
+#Write csv data locally -- read write permissions (old files will be overwritten with the w+ permission paramater)
+csvfile = open('fb_daily.csv', 'w+')
+formattedCSV = open('fb_formatted.csv','w+')
+csvfile.write(data)
+#df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
 
-#with open('jsonDump.json', 'w') as jsonFile:
-#    json.dump(jsonData, jsonFile)
+csvRead = csv.reader(csvfile)
+tempCsv = csv.writer(formattedCSV)
+for row in csvRead:
+    if any(row):
+        tempCsv.writerow(row)
+formattedCSV.close()
+csvfile.close()
 
-#csvData = jsonData['Time Series (Daily)']
-with open('fb_daily.csv','w') as csvfile:
-    csvfile.write(data)
+#reopen csvfile with read permissions for pandas
+csvfile = open('fb_daily.csv', 'r+')
+df = pd.read_csv('fb_daily.csv')
 
-#print(str(csvData))
+df = df.sort_values('timestamp')
+
+print(df.head())
+
+
+# First calculate the mid prices from the highest and lowest
+high_prices = df['high'].to_numpy()
+low_prices = df['low'].to_numpy()
+mid_prices = (high_prices+low_prices)/2.0
+
+print(mid_prices.shape)
+
+trainData = mid_prices[:1900]
+testData = mid_prices[1900:]
+
+print(testData.shape)
+
+scaler = sklearn.preprocessing.MinMaxScaler()
+train_data = train_data.reshape(-1,1)
+test_data = test_data.reshape(-1,1)
